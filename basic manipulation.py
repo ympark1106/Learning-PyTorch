4acb1ceaab0f12715b8da85a7d1632df5661bd10
ympark1106 <ympark1106@gmail.com>
2023-09-24 13:45:28 +0900

    기본 연산 공부 끝

--- basic manipulation.py
+++ basic manipulation.py
@@ -124,3 +124,42 @@ print(lt)
 # %%
 print(lt.float())
 # %%
+bt = torch.ByteTensor([True, False, False,True])  
+print(bt)
+# %%
+print(bt.long())
+print(bt.float())
+# %%
+#concatenate
+x = torch.FloatTensor([[1,2],[3,4]])
+y = torch.FloatTensor([[5,6],[7,8]])
+
+print(torch.cat([x,y], dim = 0))
+print(torch.cat([x,y], dim = 1))
+
+# %%
+#Stacking
+x=  torch.FloatTensor([1,4])
+y=  torch.FloatTensor([2,5])
+z=  torch.FloatTensor([3,6])
+#%%
+print(torch.stack([x,y,z]))
+print(torch.stack([x,y,z], dim=1))
+# %%
+print(torch.cat([x.unsqueeze(0), y.unsqueeze(0), z.unsqueeze(0)], dim = 0))
+# %%
+#Ones and Zeros
+x = torch.FloatTensor([[0,1,2], [2,1,0]])
+print(x)
+# %%
+print(torch.ones_like(x))
+print(torch.zeros_like(x))
+# %%
+#In-place Operation 
+x = torch.FloatTensor([[1,2], [3,4]])
+# %%
+print(x.mul(2.))
+print(x)
+print(x.mul_(2.)) # 'mul_'는 메모리에 새로운 tensor를 할당하지 않고 연산 수행 가능
+print(x)
+# %%
