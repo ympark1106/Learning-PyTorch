83f85a8908443124ba2dae4e7360112deb4349af
ympark1106 <ympark1106@gmail.com>
2023-09-28 16:43:06 +0900

    second commit

--- Linear Regression.py
+++ Linear Regression.py
@@ -1,7 +1,9 @@
-#%%
-print("Hello Linear Regression")
 # %%
+import numpy as np
+import torch
 #linear regression에서는 loss를 MSE로 계산
 # %%
 x_train = torch.FloatTensor([[1],[2],[3]])
 y_train = torch.FloatTensor([[4],[5],[6]])
+
+# %%
